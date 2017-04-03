fd47aad1ddb244c4c098e71a1184e4f440e36cbd
JFO <jfornech@gmail.com>
2017-04-03 06:09:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -3,8 +3,7 @@ gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
 
 global appreciation
-appreciation = {'trimestre':'', 'investissement':'', 'travail':'', 'comportement':''}
-
+appreciation = {'trimestre':'', 'investissement':'', 'travail':'', 'comportement':'', 'conseil':''}
 
 
 class Handler:
@@ -13,10 +12,11 @@ class Handler:
     def onDeleteWindow(self, *args):
         Gtk.main_quit(*args)
 
-    #def nom_eleve_changed(self):
-        #nom_eleve = text_nom_eleve.get_text()
-        #print (nom_eleve)
+    def on_about(self, button):
+        about.show_all()
 
+    def on_quitter(self, *args):
+        Gtk.main_quit(*args)
 
     def on_checkbutton_trimestre_clicked (self, button ):
         trimestre = button.get_label()
@@ -24,7 +24,16 @@ class Handler:
             appreciation['trimestre'] = trimestre
         else:
             appreciation['trimestre'] = ""
-        text.set_text(appreciation.get('trimestre') + ' ' + appreciation.get('investissement')+ ' ' + appreciation.get('travail')+ ' ' + appreciation.get('comportement'))
+        text.set_text(appreciation.get('trimestre') + ', ' + appreciation.get('investissement')+ '. ' + appreciation.get('travail')+ '. ' + appreciation.get('comportement')+ '. ' + appreciation.get('conseil'))
+
+    def on_conseils_clicked (self, button ):
+        conseil = button.get_label()
+        if button.get_active():
+            appreciation['conseil'] = conseil
+        else:
+            appreciation['conseil'] = ""
+        text.set_text(appreciation.get('trimestre') + ', ' + appreciation.get('investissement')+ '. ' + appreciation.get('travail')+ '. ' + appreciation.get('comportement')+ '. ' + appreciation.get('conseil'))
+
 
     def on_checkbutton_investissement_clicked(self, button):
         investissement = button.get_label()
@@ -32,10 +41,8 @@ class Handler:
             appreciation['investissement'] = investissement
         else:
             appreciation['investissement'] = ""
+        text.set_text(appreciation.get('trimestre') + ', ' + appreciation.get('investissement')+ '. ' + appreciation.get('travail')+ '. ' + appreciation.get('comportement')+ '. ' + appreciation.get('conseil'))
 
-        text.set_text(
-            appreciation.get('trimestre') + ' ' + appreciation.get('investissement') + ' ' + appreciation.get(
-                'travail') + ' ' + appreciation.get('comportement'))
 
     def on_checkbutton_comportement_clicked (self, button ):
         comportement = button.get_label()
@@ -44,7 +51,7 @@ class Handler:
         else:
             appreciation['comportement'] = ""
 
-        text.set_text(appreciation.get('trimestre') + ' ' + appreciation.get('investissement')+ ' ' + appreciation.get('travail')+ ' ' + appreciation.get('comportement'))
+        text.set_text(appreciation.get('trimestre') + ', ' + appreciation.get('investissement')+ '. ' + appreciation.get('travail')+ '. ' + appreciation.get('comportement')+ '. ' + appreciation.get('conseil'))
 
     def on_checkbutton_travail_clicked (self, button ):
         travail = button.get_label()
@@ -53,21 +60,21 @@ class Handler:
         else:
             appreciation['travail'] = ""
 
-        text.set_text(appreciation.get('trimestre') + ' ' + appreciation.get('investissement')+ ' ' + appreciation.get('travail')+ ' ' + appreciation.get('comportement'))
+        text.set_text(appreciation.get('trimestre') + ', ' + appreciation.get('investissement')+ '. ' + appreciation.get('travail')+ '. ' + appreciation.get('comportement')+ '. ' + appreciation.get('conseil'))
 
 
 
 builder = Gtk.Builder()
 builder.add_from_file("bulletin_test.glade")
 builder.connect_signals(Handler())
+about = builder.get_object("aboutdialog")
 
 window = builder.get_object("window1")
 window.connect("delete-event", Gtk.main_quit)
 #window = Gtk.Window(title='Hello world')
 
 text = builder.get_object("entry1")
-#text_nom_eleve = builder.get_object("nom_eleve")
-
+text_nom_eleve = builder.get_object("nom_eleve")
 
 window.show_all()
 Gtk.main()
